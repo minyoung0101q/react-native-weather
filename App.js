9327bc8fa07f21c37821f8a5a3be5b00e6f2e697
stylish_code <minyoung0101q@gmail.com>
2024-01-13 15:03:12 +0900

    layout system

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+// React Native에서 View 컴포넌트를 import한다.
+import { View } from "react-native";
+// App 이라는 이름의 함수형 컴포넌트 정의, 이 컴포넌트는 애플리케이션의 주 진입점이 되는 컴포넌트로, 기본적으로 내보내진다.
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    // React Native의 View 컴포넌트를 사용하여 부모 컨테이너를 만든다.
+    <View style={{ flex: 50, flexDirection: "row" }}>
+      {/* 첫 번째 자식 View 컴포넌트 생성 */}
+      <View style={{ flex: 20, backgroundColor: "tomato" }}></View>
+      <View style={{ flex: 20, backgroundColor: "teal" }}></View>
+      <View style={{ flex: 10, backgroundColor: "orange" }}></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
