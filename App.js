d7dc70cb22e9f86b37eb32319f62f7f6a22b3c66
stylish_code <minyoung0101q@gmail.com>
2024-01-13 22:57:41 +0900

    styles

--- App.js
+++ App.js
@@ -1,14 +1,50 @@
-// React Native에서 View 컴포넌트를 import한다.
-import { View } from "react-native";
-// App 이라는 이름의 함수형 컴포넌트 정의, 이 컴포넌트는 애플리케이션의 주 진입점이 되는 컴포넌트로, 기본적으로 내보내진다.
+import { StatusBar } from "expo-status-bar";
+import { View, Text, StyleSheet } from "react-native";
+
 export default function App() {
   return (
-    // React Native의 View 컴포넌트를 사용하여 부모 컨테이너를 만든다.
-    <View style={{ flex: 50, flexDirection: "row" }}>
-      {/* 첫 번째 자식 View 컴포넌트 생성 */}
-      <View style={{ flex: 20, backgroundColor: "tomato" }}></View>
-      <View style={{ flex: 20, backgroundColor: "teal" }}></View>
-      <View style={{ flex: 10, backgroundColor: "orange" }}></View>
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      {/* styles.weather 이것이 모든 예보의 Container가 되는 것이 목표 */}
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "tomato",
+  },
+  city: {
+    flex: 1.2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: "500",
+  },
+  weather: {
+    flex: 3,
+  },
+  day: {
+    flex: 1,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
+  },
+});
