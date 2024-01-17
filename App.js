29ee7c7ea52353b3f0914f807c3be5740ebb931e
stylish_code <minyoung0101q@gmail.com>
2024-01-17 18:31:01 +0900

    styles part two

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import { StatusBar } from "expo-status-bar";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ScrollView, Dimensions } from "react-native";
+
+const SCREEN_WIDTH = Dimensions.get("window").width;
+
+console.log(SCREEN_WIDTH);
 
 export default function App() {
   return (
@@ -8,12 +12,29 @@ export default function App() {
         <Text style={styles.cityName}>Seoul</Text>
       </View>
       {/* styles.weather 이것이 모든 예보의 Container가 되는 것이 목표 */}
-      <View style={styles.weather}>
+      <ScrollView
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
         <View style={styles.day}>
           <Text style={styles.temp}>27</Text>
           <Text style={styles.description}>Sunny</Text>
         </View>
-      </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -32,11 +53,9 @@ const styles = StyleSheet.create({
     fontSize: 68,
     fontWeight: "500",
   },
-  weather: {
-    flex: 3,
-  },
+  weather: {},
   day: {
-    flex: 1,
+    width: SCREEN_WIDTH,
     alignItems: "center",
   },
   temp: {
